64589718e1541a80b52b0a128ff4ff53359d3e03
jjl <3177738100@qq.com>
2022-09-22 19:09:45 +0800

    third-commit

--- git-test/src/main/java/com/atguigu/git/GItTest.java
+++ git-test/src/main/java/com/atguigu/git/GItTest.java
@@ -16,5 +16,6 @@ public class GItTest {
         System.out.println("gitee test!");
         System.out.println("gitee test2!");
         System.out.println(".............");
+        System.out.println("hello git");
     }
 }
